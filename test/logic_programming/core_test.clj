79e66163a6887fdd2ff7cb3db5f004ac2d40e754
WON SEOB SEO <pghoou@hotmail.com>
2017-12-14 20:24:05 +0200

    Add ex2

--- test/logic_programming/core_test.clj
+++ test/logic_programming/core_test.clj
@@ -6,4 +6,12 @@
 (fact
   "ex1"
   (log/run* [q]
-            (log/== q true)) => [true])
+            (log/== q true))
+  => [true])
+
+(fact
+  "ex2"
+  (log/run* [q]
+            (log/membero q [1 2 3])
+            (log/membero q [2 3 4]))
+  => [2 3])

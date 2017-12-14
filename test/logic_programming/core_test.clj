ba60b579975dba967ccc5965e6c93e724885874e
WON SEOB SEO <pghoou@hotmail.com>
2017-12-14 20:42:48 +0200

    Add ex3 and ex4

--- test/logic_programming/core_test.clj
+++ test/logic_programming/core_test.clj
@@ -15,3 +15,15 @@
             (log/membero q [1 2 3])
             (log/membero q [2 3 4]))
   => [2 3])
+
+(fact
+  "ex3"
+  (log/run* [query-variable]
+            (log/== 1 1))
+  => '[_0])
+
+(fact
+  "ex4"
+  (log/run* [query-variable]
+            (log/!= query-variable 1))
+  => '((_0 :- (!= (_0 1)))))
